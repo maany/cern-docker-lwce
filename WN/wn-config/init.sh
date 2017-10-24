742fd669839243326289c36d16e523d81292e6a6
maany <imptodefeat@gmail.com>
2017-10-24 17:43:40 +0200

    add YAIM configuration files for WN

--- WN/wn-config/init.sh
+++ WN/wn-config/init.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 # move configuration files to the correct place
+echo "Copying configuraiton files..."
+cp /etc/opt/wn-config/wn-info.def /root/
+cp /etc/opt/wn-config/users.conf /root/
+cp /etc/opt/wn-config/groups.conf /root/
+cp /etc/opt/wn-config/wn-list.conf /root/
 
 # run YAIM
-echo ""
+echo "Starting YAIM..."
+/opt/glite/yaim/bin/yaim -c \
+	-s /wn-config/wn-info.def \
+	-n WN \
+	-n TORQUE_client
