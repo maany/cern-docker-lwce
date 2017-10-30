a432240f99be4a3ae8b6e65521244a6f6eedd8d3
maany <imptodefeat@gmail.com>
2017-10-30 11:56:54 +0100

    Showcase : WN hello world image ready

--- WN/wn-config/init.sh
+++ WN/wn-config/init.sh
@@ -2,10 +2,10 @@
 
 # move configuration files to the correct place
 echo "Copying configuraiton files..."
-cp /etc/opt/wn-config/wn-info.def /root/
-cp /etc/opt/wn-config/users.conf /root/
-cp /etc/opt/wn-config/groups.conf /root/
-cp /etc/opt/wn-config/wn-list.conf /root/
+cp /wn-config/wn-info.def /root/
+cp /wn-config/users.conf /root/
+cp /wn-config/groups.conf /root/
+cp /wn-config/wn-list.conf /root/
 
 # run YAIM
 echo "Starting YAIM..."
